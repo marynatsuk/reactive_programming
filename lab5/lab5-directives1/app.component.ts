91cd91c15f0e301968d39a2ea1b278bb4804c479
Maryna Tsuk <73252923+marynatsuk@users.noreply.github.com>
2023-11-02 19:44:18 +0200

    Create app.component.ts

---
+++ lab5/lab5-directives1/app.component.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: `
+<div [ngClass]="{verdanaFont:isVerdana}">
+<h1>Hello Angular</h1>
+<p [ngClass]="{segoePrintFont:isSegoe}">
+Angular представляє модульну архітектуру додатку
+</p>
+</div>
+<div [ngClass]="currentClasses">
+<h1>Hello Angular</h1>
+<p>
+Angular представляє модульну архітектуру додатку
+</p>
+</div>
+<div [ngStyle]="{'font-size':'13px', 'font-family':'Verdana', 'color':'green'}">
+<h1>Вивчаємо директиви</h1>
+<p [ngStyle]="{'font-size':'16px', 'font-family':'Segoe Print'}">
+Використання атрибутивної директиви [ngStyle]
+</p>
+</div>
+ <div [ngClass]="{invisible: visibility}">
+ <h1> Вивчаємо директиви </h1>
+ <p>
+ Використання динамічної зміни стилів
+ </p>
+ </div>
+ <button (click)="toggle()">Toggle</button>`,
+    styles: [`
+.verdanaFont{font-size:15px; font-family:Verdana;}
+.segoePrintFont{font-size:16px; font-family:"Segoe Print";}
+.navyColor{color:navy;}
+.invisible{display:none;}
+`]
+})
+export class AppComponent {
+    isVerdana = true;
+    isSegoe = true;
+    isNavy = true;
+    visibility = true;
+    currentClasses = {
+        verdanaFont: this.isVerdana,
+    navyColor: this.isNavy, 
+        
+    }
+    toggle() {
+        this.visibility = !this.visibility;
+    }
+}
